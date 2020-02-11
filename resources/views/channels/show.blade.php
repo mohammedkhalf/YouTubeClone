bdf91f7a3c77c5125aa987e474f03c1e37d787cc
mohammed ali <coder_khalf@mu.edu.sa>
2020-02-11 11:58:12 +0300

    Conditional rendering with blade and inline vue temlaltes

--- resources/views/channels/show.blade.php
+++ resources/views/channels/show.blade.php
@@ -32,9 +32,9 @@
                             @endif
 
                                 <div class="text-center">
-                                        <subscribe-button   :subscriptions="{{ $channel->subscriptions }}" inline-template>
+                                        <subscribe-button  :channel="{{$channel}}" :subscriptions="{{ $channel->subscriptions }}" inline-template>
                                             <button  @click="toggleSubscription"  class="btn btn-danger">
-                                                Subscribe 7K
+                                                @{{ owner ? '' : subscribed ? 'UnSubscribe' : 'Subscripe' }}  @{{ subscriptions.length }}  @{{ owner ? 'Subscribers' : '' }}
                                             </button>
                                         </subscribe-button>
                                 </div>
